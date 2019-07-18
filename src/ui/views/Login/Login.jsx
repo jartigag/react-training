88be90c520bc625215c3087a9bc416de6319cd79
Mikel Ros <mikelros22@gmail.com>
2019-07-18 12:58:06 +0200

    Implement user login and add security to login route

--- src/ui/views/Login/Login.jsx
+++ src/ui/views/Login/Login.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { BoxColumn, ButtonRow, Container, Input, LoginBox, Logo } from './Login.style'
+import { BoxColumn, ButtonRow, Container, Form, Input, LoginBox, Logo } from './Login.style'
 import { Button } from 'ui/components/Button/Button'
 import { Text } from 'ui/components/Text'
 import { Redirect } from 'react-router-dom'
 import { routes } from 'ui/routes'
 import reactLogo from './react-logo.png'
+import { UserService } from 'core/services/User'
 
 export const Login = ({ location }) => {
   const [user, setUser] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [redirectToReferrer, setRedirectToReferrer] = React.useState(false)
   const { from } = location.state || { from: { pathname: routes.COMICS } }
+  const onLogin = async (user, password) => {
+    await UserService.login(user, password)
+    setRedirectToReferrer(true)
+  }
 
   if (redirectToReferrer) {
     return <Redirect to={from} />
@@ -24,18 +29,24 @@ export const Login = ({ location }) => {
           <Text marginTop="extrasmall" marginBottom="extrasmall" as="span" textAlign="center" size="base">
             Iniciar sesión
           </Text>
-          <Input value={user} onChange={e => setUser(e.target.value)} placeholder="Usuario" />
-          <Input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-            placeholder="Contraseña"
-          />
-          <ButtonRow>
-            <Button type="submit" size="small">
-              Acceder
-            </Button>
-          </ButtonRow>
+          <Form
+            onSubmit={event => {
+              event.preventDefault()
+              onLogin(user, password)
+            }}>
+            <Input value={user} onChange={e => setUser(e.target.value)} placeholder="Usuario" />
+            <Input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+              placeholder="Contraseña"
+            />
+            <ButtonRow>
+              <Button type="submit" size="small">
+                Acceder
+              </Button>
+            </ButtonRow>
+          </Form>
         </LoginBox>
       </BoxColumn>
     </Container>
--- src/ui/views/Root.jsx
+++ src/ui/views/Root.jsx
@@ -3,12 +3,15 @@ import { GlobalStyles } from 'ui/theme/GlobalStyles'
 import { ComicsList } from 'ui/views/ComicsList'
 import { Route, Switch } from 'react-router-dom'
 import { routes } from 'ui/routes'
+import { PrivateRoute } from 'ui/views/_components/Routing/PrivateRoute'
+import { Login } from 'ui/views/Login'
 
 export const Root = () => (
   <>
     <GlobalStyles />
     <Switch>
-      <Route exact path={routes.COMICS} component={ComicsList} />
+      <PrivateRoute exact path={routes.COMICS} component={ComicsList} />
+      <Route exact path={routes.LOGIN} component={Login} />
     </Switch>
   </>
 )

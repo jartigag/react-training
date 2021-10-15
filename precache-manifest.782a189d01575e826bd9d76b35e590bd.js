1363c2d2fbdf37a3f06e209bd160394ef96902ca
jartigag <jartigag@pm.me>
2021-10-15 21:40:03 +0200

    Updates

---
+++ precache-manifest.782a189d01575e826bd9d76b35e590bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b7e0378565c97b3ed78df968e6ca06",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "761021d45619c94f1c4c",
+    "url": "/react-training/static/js/2.ecae2784.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-training/static/js/2.ecae2784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05e11f2f79d08ceb423",
+    "url": "/react-training/static/js/main.3fedecfc.chunk.js"
+  },
+  {
+    "revision": "6aa49adc647e8b171484",
+    "url": "/react-training/static/js/runtime-main.497f2de0.js"
+  }
+]);
